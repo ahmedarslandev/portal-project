2a9160c2c05e3b0fc6406b8ef1174bf45896dbc0
ahmedarslandev <148966385+ahmedarslandev@users.noreply.github.com>
2024-05-27 05:35:02 +0500

    ss,ss,s

--- src/app/api/get-user-details/route.ts
+++ src/app/api/get-user-details/route.ts
@@ -5,33 +5,27 @@ import { connectDB } from "@/dbConfig/connectDB";
 
 export async function GET(req: NextRequest) {
   try {
-    // await connectDB();
-    // const token = req;
+    await connectDB();
+    const token = req.cookies.get("token");
 
-    return NextResponse.json({
-      success: true,
-      message: "Successfully logged in",
-      status: 200,
-      token: req,
-    });
-    // if (!token) {
-    //   return NextResponse.json({
-    //     success: false,
-    //     message: "Token not found",
-    //     status: 401,
-    //   });
-    // }
-    // const { id } = jwt.decode(token.value as any) as JwtPayload;
+    if (!token) {
+      return NextResponse.json({
+        success: false,
+        message: "Token not found",
+        status: 401,
+      });
+    }
+    const { id } = jwt.decode(token.value as any) as JwtPayload;
 
-    // const user = await userModel.findById(id);
-    // if (!user) {
-    //   return NextResponse.json({
-    //     success: false,
-    //     message: "Token is invalid",
-    //     status: 404,
-    //   });
-    // }
-    // return NextResponse.json(user);
+    const user = await userModel.findById(id);
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        message: "Token is invalid",
+        status: 404,
+      });
+    }
+    return NextResponse.json(user);
   } catch (error) {
     return NextResponse.json({
       success: false,
