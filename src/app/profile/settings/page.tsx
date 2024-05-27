3c2509643a813c63eb5e551c625d06255b808061
ahmedarslandev <148966385+ahmedarslandev@users.noreply.github.com>
2024-05-27 22:53:42 +0500

    cndjndjndjd

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -3,7 +3,10 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Page = () => {
-  const [userDetails, setUserDetails] = useState(Object);
+  interface res {
+    message: string;
+  }
+  const [userDetails, setUserDetails] = useState<res>(Object);
   useEffect(() => {
     const res = axios.get(
       "api/fetch-callerId?email=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFobWVkYXJzbGFuYXJzbGFuOUBnbWFpbC5jb20iLCJpYXQiOjE3MTY3MDg3NzV9.DjFfb3ebc8uwwWQAfHpCVkvPDravcwPukqIep6_-gPM&callerId=20945675647"
@@ -23,7 +26,7 @@ const Page = () => {
       description: "User Details fetched successfully",
     });
   }, []);
-  return <div>{userDetails.message}</div>;
+  return <div>{userDetails?.message}</div>;
 };
 
 export default Page;
