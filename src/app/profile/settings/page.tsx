5deca930073100d0066c981fa238bbe468cd5521
ahmedarslandev <148966385+ahmedarslandev@users.noreply.github.com>
2024-05-27 22:50:32 +0500

    messsnsjjd

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { toast } from "@/components/ui/use-toast";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
-  return <div></div>;
+  const [userDetails, setUserDetails] = useState(Object);
+  useEffect(() => {
+    const res = axios.get(
+      "api/fetch-callerId?email=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFobWVkYXJzbGFuYXJzbGFuOUBnbWFpbC5jb20iLCJpYXQiOjE3MTY3MDg3NzV9.DjFfb3ebc8uwwWQAfHpCVkvPDravcwPukqIep6_-gPM&callerId=20945675647"
+    );
+    res.then((response) => {
+      const data = response.data;
+      setUserDetails(data);
+    });
+    console.log(userDetails);
+    res.catch((error) => {
+      console.log(error);
+      return;
+    });
+    res.finally(() => {});
+    toast({
+      title: "User Details",
+      description: "User Details fetched successfully",
+    });
+  }, []);
+  return <div>{userDetails.message}</div>;
 };
 
 export default Page;
