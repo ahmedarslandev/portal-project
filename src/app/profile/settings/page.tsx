f6ed4ea0b7a2ca48b2b593f0d455fb9488a3d61f
ahmedarslandev <148966385+ahmedarslandev@users.noreply.github.com>
2024-05-28 12:41:39 +0500

    dndjndjdjndjd

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -1,33 +1,22 @@
-"use client";
-
 import { toast } from "@/components/ui/use-toast";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
-const Page = () => {
-  interface Res {
-    message: string;
-    callerId?: string;
-  }
-  const [userDetails, setUserDetails] = useState<Res>({ message: "" });
+interface Res {
+  message: string;
+  callerId?: string;
+}
 
+interface PageProps {
+  userDetails: Res;
+}
+
+const Page = ({ userDetails }: PageProps) => {
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "/api/fetch-callerId?email=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFobWVkYXJzbGFuYXJzbGFuOTFAZ21haWwuY29tIiwiaWF0IjoxNzE2ODUzMDUxfQ.PqiuH-DsSWMcy4tDJqrGMfI1MwHZwhCeVTqGr0Xs0HI&callerId=20935632563"
-        );
-        setUserDetails(response.data);
-        toast({
-          title: "User Details",
-          description: "User Details fetched successfully",
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
+    toast({
+      title: "User Details",
+      description: "User Details fetched successfully",
+    });
   }, []);
 
   return (
@@ -37,6 +26,28 @@ const Page = () => {
       {userDetails.callerId && <div>Caller ID: {userDetails.callerId}</div>}
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const response = await axios.get(
+      "https://testing-dun-pi.vercel.app/api/fetch-callerId?email=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFobWVkYXJzbGFuYXJzbGFuOTFAZ21haWwuY29tIiwiaWF0IjoxNzE2ODUzMDUxfQ.PqiuH-DsSWMcy4tDJqrGMfI1MwHZwhCeVTqGr0Xs0HI&callerId=20935632563"
+    );
+    return {
+      props: {
+        userDetails: response.data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        userDetails: {
+          message: "Error fetching data",
+        },
+      },
+    };
+  }
 };
 
 export default Page;
